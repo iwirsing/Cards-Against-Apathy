6fb92ee2cca0af65d0d7a113ed97f5f19faf2a73
Mark Calcagno <mcalcagn@gmail.com>
2022-11-03 20:54:18 -0500

    added cardio javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-// Here's a comment
+var myHeaders = new Headers();
+var cardio = 'cardio';
+myHeaders.append("x-api-key", "R78wAd5UBLglet+gIcUCSQ==qefnWKvG8uC3WfIv");
+
+var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+};
+
+fetch('https://api.api-ninjas.com/v1/exercises?type=' + cardio, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
